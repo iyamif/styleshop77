2bff703fd525e1258b8e5e489f3b056a87afed24
andreasjulio11 <andreasjulio94@gmail.com>
2024-06-22 16:34:30 +0700

    Update Order.php

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    protected $fillable = ['shop_id', 'order_code', 'name', 'phone', 'address', 'note', 'total', 'status'];
+    protected $fillable = ['shop_id', 'order_code', 'name', 'phone', 'address', 'note', 'total', 'status','status_payment'];
 
     public function shop(){
         return $this->belongsTo(Shop::class);
