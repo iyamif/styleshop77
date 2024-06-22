928cfce77f019e1f4ce6d014645e3b804dee90a7
andreasjulio11 <andreasjulio94@gmail.com>
2024-06-22 16:34:25 +0700

    Update 2022_07_06_093236_create_orders_table.php
    add kolom

--- database/migrations/2022_07_06_093236_create_orders_table.php
+++ database/migrations/2022_07_06_093236_create_orders_table.php
@@ -23,6 +23,7 @@ return new class extends Migration
             $table->text('note')->nullable();
             $table->bigInteger('total');
             $table->integer('status');
+            $table->text('status_payment');
 
             $table->foreign('shop_id')->references('id')->on('shops')->onUpdate('cascade')->onDelete('cascade');
 
